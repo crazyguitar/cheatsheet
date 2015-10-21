e5e74aea92602ff3f23dc6fb8028385f46de92ae
chang-ning <spiderpower02@gmail.com>
2015-10-21 08:14:20 +0800

    Rewrite project to a webapp via flask

---
+++ server/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, abort, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+   return render_template('index.html') 
+
+@app.route('/<string:page>',methods=['GET'])
+def cheatsheet(page):
+   try:
+      return render_template('cheatsheets/{0}.html'.format(page)) 
+   except:
+      abort(404)
+
