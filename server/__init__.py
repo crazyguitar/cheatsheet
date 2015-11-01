1415b06af1145314c8b5bec6ce5e4ce45ff2d27b
chang-ning <spiderpower02@gmail.com>
2015-11-01 14:38:39 +0800

    Add web-site testcase

--- server/__init__.py
+++ server/__init__.py
@@ -2,14 +2,15 @@ from flask import Flask, abort, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def index():
-   return render_template('index.html') 
+    return render_template('index.html')
 
-@app.route('/<string:page>',methods=['GET'])
+
+@app.route('/<string:page>', methods=['GET'])
 def cheatsheet(page):
-   try:
-      return render_template('cheatsheets/{0}.html'.format(page)) 
-   except:
-      abort(404)
-
+    try:
+        return render_template('cheatsheet/{0}.html'.format(page))
+    except:
+        abort(404)
