f342cce2ee7dc5cf1f9d9b08a61674b6292f9d26
chang-ning <spiderpower02@gmail.com>
2015-11-22 01:46:35 +0800

    Add document for server module

--- server/__init__.py
+++ server/__init__.py
@@ -1,3 +1,7 @@
+"""
+This module is cheatsheet website backend.
+"""
+
 from flask import Flask, abort, render_template
 
 app = Flask(__name__)
@@ -5,11 +9,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
+    """ Index page API """
     return render_template('index.html')
 
 
 @app.route('/<string:page>', methods=['GET'])
 def cheatsheet(page):
+    """ Cheat sheet API """
     try:
         return render_template('cheatsheet/{0}.html'.format(page))
     except:
