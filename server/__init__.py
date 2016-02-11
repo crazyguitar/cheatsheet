00175970c985ea48ae1f8f152b24f625d2d453dd
chang-ning <spiderpower02@gmail.com>
2016-02-11 15:11:32 +0800

    Test https redirection

--- server/__init__.py
+++ server/__init__.py
@@ -3,9 +3,10 @@ This module is cheatsheet website backend.
 """
 
 from flask import Flask, abort, render_template
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
-
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.route('/', methods=['GET'])
 def index():
