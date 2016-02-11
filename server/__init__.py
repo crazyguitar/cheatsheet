2df36f0eebf368ee00b69c2cb37ece32695a2f13
chang-ning <spiderpower02@gmail.com>
2016-02-11 14:19:43 +0800

    Remove validated file

--- server/__init__.py
+++ server/__init__.py
@@ -20,11 +20,3 @@ def cheatsheet(page):
         return render_template('cheatsheet/{0}.html'.format(page))
     except:
         abort(404)
-
-@app.route('/78FA73EC439FE689CE23876A72F75D5A.txt')
-def ssl_hash():
-    """ ssl has file """
-    try:
-        return render_template('78FA73EC439FE689CE23876A72F75D5A.txt')
-    except:
-        abort(404)
