05ca1b621a834e9698bcc563d335fc9166870e84
chang-ning <spiderpower02@gmail.com>
2016-02-28 16:51:35 +0800

    Enable github fork me badge

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -117,7 +117,13 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'show_powered_by': False,
+    'github_user': 'crazyguitar',
+    'github_repo': 'cheatsheet',
+    'github_banner': True,
+    'show_related': False
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
